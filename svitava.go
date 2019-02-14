86f3a76c18d6cd9d2f70995281a3219f47746b8a
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-14 12:40:24 +0100

    CLI arguments parsing

--- svitava.go
+++ svitava.go
@@ -13,6 +13,23 @@
 package main
 
 // import "svitava/renderer"
+import (
+	"flag"
+)
 
 func main() {
+	var width int
+	var height int
+	var aa bool
+
+	flag.IntVar(&width, "w", 0, "image width (shorthand)")
+	flag.IntVar(&width, "width", 0, "image width")
+
+	flag.IntVar(&height, "h", 0, "image height (shorthand)")
+	flag.IntVar(&height, "height", 0, "image height")
+
+	flag.BoolVar(&aa, "a", false, "enable antialiasing (shorthand)")
+	flag.BoolVar(&aa, "antialias", false, "enable antialiasing")
+
+	flag.Parse()
 }
