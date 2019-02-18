60503d65c77637d22c95b3cb6c2eacc3fdde39ce
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-18 09:06:00 +0100

    Bunch of fixes, main renamed, channel used to sync computation etc.

--- renderer/mandelbrot.go
+++ renderer/mandelbrot.go
@@ -1,6 +1,6 @@
 package renderer
 
-func calcMandelbrot(width uint, height uint, maxiter uint, palette [][3]byte, image []byte, cy float64) {
+func calcMandelbrot(width uint, height uint, maxiter uint, palette [][3]byte, image []byte, cy float64, done chan bool) {
 	var cx float64 = -2.0
 	for x := uint(0); x < width; x++ {
 		var zx float64 = 0.0
@@ -22,11 +22,12 @@ func calcMandelbrot(width uint, height uint, maxiter uint, palette [][3]byte, im
 		image[3*x+2] = color[2]
 		cx += 3.0 / float64(width)
 	}
+	done <- true
 }
 
 func calcMandelbrotComplex(width uint, height uint, maxiter uint, palette [][3]byte, image []byte, cy float64, done chan bool) {
 	var c complex128 = complex(-2.0, cy)
-	var dc complex128 = complex(3.0 / float64(width), 0.0)
+	var dc complex128 = complex(3.0/float64(width), 0.0)
 	for x := uint(0); x < width; x++ {
 		var z complex128 = 0.0 + 0.0i
 		var i uint = 0
@@ -48,44 +49,22 @@ func calcMandelbrotComplex(width uint, height uint, maxiter uint, palette [][3]b
 	done <- true
 }
 
-func main() {
-	if len(os.Args) < 4 {
-		println("usage: ./mandelbrot width height maxiter")
-		os.Exit(1)
-	}
-
-	width, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Printf("Improper width parameter: '%s'\n", os.Args[1])
-		os.Exit(1)
-	}
-
-	height, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		fmt.Printf("Improper height parameter: '%s'\n", os.Args[2])
-		os.Exit(1)
-	}
-
-	maxiter, err := strconv.Atoi(os.Args[3])
-	if err != nil {
-		fmt.Printf("Improper maxiter parameter: '%s'\n", os.Args[3])
-		os.Exit(1)
-	}
-
+func RenderMandelbrotFractal(width uint, height uint, maxiter uint, palette [][3]byte) []byte {
 	done := make(chan bool, height)
 
 	image := make([]byte, width*height*3)
-	offset := 0
+	offset := uint(0)
 	delta := width * 3
 
 	var cy float64 = -1.5
-	for y := 0; y < height; y++ {
-		go calcMandelbrot(uint(width), uint(height), uint(maxiter), mandmap[:], image[offset:offset+delta], cy, done)
+	for y := uint(0); y < height; y++ {
+		go calcMandelbrot(width, height, maxiter, palette, image[offset:offset+delta], cy, done)
 		offset += delta
 		cy += 3.0 / float64(height)
 	}
-	for i := 0; i < height; i++ {
+	for i := uint(0); i < height; i++ {
+		println(i)
 		<-done
 	}
-	writeImage(uint(width), uint(height), image)
+	return image
 }
