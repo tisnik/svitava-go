3a809d2117f6c08e13dd9e151576c803ded37952
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-11 09:06:52 +0100

    Constructor for ZImage

--- renderer/cplx/zimage.go
+++ renderer/cplx/zimage.go
@@ -1,3 +1,11 @@
 package cplx
 
-type ZImage [][]cplx.ZPixel
+type ZImage [][]ZPixel
+
+func NewZImage(width uint, height uint) ZImage {
+        zimage := make([][]ZPixel, height)
+        for i := uint(0); i<height; i++ {
+                zimage[i] = make([]ZPixel, width)
+        }
+	return zimage
+}
