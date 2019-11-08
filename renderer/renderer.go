f1d261bbf97ea328f2033cdf658dc90d23004c90
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-08 09:02:36 +0100

    Refactoring: complexImageToImage

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -16,18 +16,7 @@ func waitForWorkers(done chan bool, height uint) {
 	}
 }
 
-func RenderMandelbrotFractal(width uint, height uint, pcx float64, pcy float64, maxiter uint, palette [][3]byte) image.Image {
-	done := make(chan bool, height)
-
-	zimage := cplx.NewZImage(width, height)
-
-	var cy float64 = -1.5
-	for y := uint(0); y < height; y++ {
-		go cplx.CalcMandelbrot(width, height, pcx, pcy, maxiter, zimage[y], cy, done)
-		cy += 3.0 / float64(height)
-	}
-	waitForWorkers(done, height)
-
+func complexImageToImage(zimage cplx.ZImage, width uint, height uint, palette [][3]byte) image.Image {
 	image := image.NewNRGBA(image.Rect(0, 0, int(width), int(height)))
 
 	for y := 0; y < int(height); y++ {
@@ -44,8 +33,22 @@ func RenderMandelbrotFractal(width uint, height uint, pcx float64, pcy float64, 
 			offset++
 		}
 	}
-
 	return image
+}
+
+func RenderMandelbrotFractal(width uint, height uint, pcx float64, pcy float64, maxiter uint, palette [][3]byte) image.Image {
+	done := make(chan bool, height)
+
+	zimage := cplx.NewZImage(width, height)
+
+	var cy float64 = -1.5
+	for y := uint(0); y < height; y++ {
+		go cplx.CalcMandelbrot(width, height, pcx, pcy, maxiter, zimage[y], cy, done)
+		cy += 3.0 / float64(height)
+	}
+	waitForWorkers(done, height)
+
+	return complexImageToImage(zimage, width, height, palette)
 }
 
 func RenderBarnsleyFractal(width uint, height uint, maxiter uint, palette [][3]byte) image.Image {
@@ -59,25 +62,8 @@ func RenderBarnsleyFractal(width uint, height uint, maxiter uint, palette [][3]b
 		cy += 4.0 / float64(height)
 	}
 	waitForWorkers(done, height)
+
+	return complexImageToImage(zimage, width, height, palette)
 	}
 
-	image := image.NewNRGBA(image.Rect(0, 0, int(width), int(height)))
-
-	for y := 0; y < int(height); y++ {
-		offset := image.PixOffset(0, y)
-		println(y)
-		for x := uint(0); x < width; x++ {
-			i := byte(zimage[y][x].Iter)
-			image.Pix[offset] = palette[i][0]
-			offset++
-			image.Pix[offset] = palette[i][1]
-			offset++
-			image.Pix[offset] = palette[i][2]
-			offset++
-			image.Pix[offset] = 0xff
-			offset++
-		}
-	}
-
-	return image
 }
