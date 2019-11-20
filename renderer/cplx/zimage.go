5ed8343ab6d31737ded473d1ad53bce943c82551
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-20 18:04:33 +0100

    Code reformatted

--- renderer/cplx/zimage.go
+++ renderer/cplx/zimage.go
@@ -3,9 +3,9 @@ package cplx
 type ZImage [][]ZPixel
 
 func NewZImage(width uint, height uint) ZImage {
-        zimage := make([][]ZPixel, height)
-        for i := uint(0); i<height; i++ {
-                zimage[i] = make([]ZPixel, width)
-        }
+	zimage := make([][]ZPixel, height)
+	for i := uint(0); i < height; i++ {
+		zimage[i] = make([]ZPixel, width)
+	}
 	return zimage
 }
