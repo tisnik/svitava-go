0d0f60bdb94fdf6e16409db426f73c0f10c1b123
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-20 13:19:30 +0100

    Fixed comments

--- image/bmp.go
+++ image/bmp.go
@@ -1,5 +1,5 @@
 package image
 
-// Write an image represented by byte slice into file with BMP format.
+// WriteBMPImage writes an image represented by byte slice into file with BMP format.
 func WriteBMPImage(filename string, width uint, height uint, raw_data []byte) {
 }
