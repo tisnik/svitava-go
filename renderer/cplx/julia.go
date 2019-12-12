53ff275828bdfc106cc1b7813641a8a5ef0848e3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-12 18:25:29 +0100

    Fixed linter issues

--- renderer/cplx/julia.go
+++ renderer/cplx/julia.go
@@ -1,11 +1,12 @@
 package cplx
 
-func CalcJulia(width uint, height uint, maxiter uint, zimage_line []ZPixel, zy0 float64, cx float64, cy float64, done chan bool) {
+// CalcJulia calculate one line of Julia set into the provided scanline of ZPixels
+func CalcJulia(width uint, height uint, maxiter uint, zimageLine []ZPixel, zy0 float64, cx float64, cy float64, done chan bool) {
 	var zx0 float64 = -2.0
 	for x := uint(0); x < width; x++ {
 		var zx float64 = zx0
 		var zy float64 = zy0
-		var i uint = 0
+		var i uint
 		for i < maxiter {
 			zx2 := zx * zx
 			zy2 := zy * zy
@@ -16,7 +17,7 @@ func CalcJulia(width uint, height uint, maxiter uint, zimage_line []ZPixel, zy0 
 			zx = zx2 - zy2 + cx
 			i++
 		}
-		zimage_line[x] = ZPixel{Iter: i, Z: complex(zx, zy)}
+		zimageLine[x] = ZPixel{Iter: i, Z: complex(zx, zy)}
 		zx0 += 4.0 / float64(width)
 	}
 	done <- true
