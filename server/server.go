b47c414f6fc7949e0e61e3716d42cab0a0613f8f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-12 18:27:47 +0100

    Added missing docstrings

--- server/server.go
+++ server/server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// StartServer starts HTTP server that provides all static and dynamic data
 func StartServer(port uint) {
 	log.Printf("Starting server on port %d", port)
 	http.Handle("/", http.FileServer(http.Dir("web-content/")))
