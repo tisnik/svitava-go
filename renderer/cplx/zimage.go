d22a87868ea7f194a829a29e64a3179007da698b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-12-12 18:32:16 +0100

    Added docstrings

--- renderer/cplx/zimage.go
+++ renderer/cplx/zimage.go
@@ -1,7 +1,9 @@
 package cplx
 
+// ZImage is representation of raster image consisting of ZPixels
 type ZImage [][]ZPixel
 
+// NewZImage constructs new instance of ZImage
 func NewZImage(width uint, height uint) ZImage {
 	zimage := make([][]ZPixel, height)
 	for i := uint(0); i < height; i++ {
