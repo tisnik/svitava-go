affaba366942eda1c739e239b73c8c965e470d37
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-04 14:35:40 +0200

    Index page

--- server/server.go
+++ server/server.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 )
 
+func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "web-content/index.html")
+}
+
 // StartServer starts HTTP server that provides all static and dynamic data
 func StartServer(port uint) {
 	log.Printf("Starting server on port %d", port)
-	http.Handle("/", http.FileServer(http.Dir("web-content/")))
-	/* http.HandleFunc("/", someHandler) */
+	/* http.Handle("/", http.FileServer(http.Dir("web-content/"))) */
+	http.HandleFunc("/", indexPageHandler)
 	http.ListenAndServe(":8080", nil)
 }
