a46f5fc178dc268223ccc05c003b4d5152749645
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-08 08:28:06 +0200

    Mandelbrot set is calculated as a whole

--- renderer/cplx/mandelbrot.go
+++ renderer/cplx/mandelbrot.go
@@ -1,48 +1,59 @@
 package cplx
 
-// CalcMandelbrot calculate one line of Mandelbrot set into the provided scanline of ZPixels
-func CalcMandelbrot(width uint, height uint, pcx float64, pcy float64, maxiter uint, zimageLine []ZPixel, cy float64, done chan bool) {
-	var cx float64 = -2.0
-	for x := uint(0); x < width; x++ {
-		var zx float64 = pcx
-		var zy float64 = pcy
-		var i uint
-		for i < maxiter {
-			zx2 := zx * zx
-			zy2 := zy * zy
-			if zx2+zy2 > 4.0 {
-				break
+// CalcMandelbrot calculates Mandelbrot set into the provided ZPixels
+func CalcMandelbrot(
+	width uint, height uint, pcx float64, pcy float64,
+	maxiter uint, zimage ZImage) {
+
+	var cy float64 = -1.5
+	for y := uint(0); y < height; y++ {
+		var cx float64 = -2.0
+		for x := uint(0); x < width; x++ {
+			var zx float64 = pcx
+			var zy float64 = pcy
+			var i uint
+			for i < maxiter {
+				zx2 := zx * zx
+				zy2 := zy * zy
+				if zx2+zy2 > 4.0 {
+					break
+				}
+				zy = 2.0*zx*zy + cy
+				zx = zx2 - zy2 + cx
+				i++
 			}
-			zy = 2.0*zx*zy + cy
-			zx = zx2 - zy2 + cx
-			i++
+			zimage[y][x] = ZPixel{Iter: i, Z: complex(zx, zy)}
+			cx += 3.0 / float64(width)
 		}
-		zimageLine[x] = ZPixel{Iter: i, Z: complex(zx, zy)}
-		cx += 3.0 / float64(width)
+		cy += 3.0 / float64(height)
 	}
-	done <- true
 }
 
-func calcMandelbrotComplex(width uint, height uint, maxiter uint, palette [][3]byte, image []byte, cy float64, done chan bool) {
-	var c complex128 = complex(-2.0, cy)
-	var dc complex128 = complex(3.0/float64(width), 0.0)
-	for x := uint(0); x < width; x++ {
-		var z complex128 = 0.0 + 0.0i
-		var i uint
-		for i < maxiter {
-			zx := real(z)
-			zy := imag(z)
-			if zx*zx+zy*zy > 4.0 {
-				break
+// CalcMandelbrotComplex calculates Mandelbrot set into the provided ZPixels
+// Calculations use complex numbers
+func CalcMandelbrotComplex(
+	width uint, height uint, pcx float64, pcy float64,
+	maxiter uint, zimage ZImage) {
+
+	var cy float64 = -1.5
+	for y := uint(0); y < height; y++ {
+		var cx float64 = -2.0
+		for x := uint(0); x < width; x++ {
+			var c complex128 = complex(cx, cy)
+			var z complex128 = complex(pcx, pcy)
+			var i uint
+			for i < maxiter {
+				zx := real(z)
+				zy := imag(z)
+				if zx*zx+zy*zy > 4.0 {
+					break
+				}
+				z = z*z + c
+				i++
 			}
-			z = z*z + c
-			i++
+			zimage[y][x] = ZPixel{Iter: i, Z: z}
+			cx += 3.0 / float64(width)
 		}
-		color := palette[i]
-		image[3*x] = color[0]
-		image[3*x+1] = color[1]
-		image[3*x+2] = color[2]
-		c += dc
+		cy += 3.0 / float64(height)
 	}
-	done <- true
 }
