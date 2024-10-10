b88aa0e95f73e51fb258c4d777fce75c48c04953
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-10 16:53:36 +0200

    Configuration loader: the stub

---
+++ configuration/configuration.go
@@ -0,0 +1,17 @@
+//
+//  (C) Copyright 2019, 2020, 2021, 2022, 2023, 2024  Pavel Tisnovsky
+//
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  which accompanies this distribution, and is available at
+//  http://www.eclipse.org/legal/epl-v10.html
+//
+//  Contributors:
+//      Pavel Tisnovsky
+//
+
+package configuration
+
+func LoadConfiguration(configFileName string) error {
+	return nil
+}
