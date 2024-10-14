a50c9adc8b0eeb3b0c21a6af466398b5aa8e4ff0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-14 09:33:36 +0200

    Barnsley Julia J2 fractal renderer

--- renderer/cplx/barnsley_julia.go
+++ renderer/cplx/barnsley_julia.go
@@ -38,3 +38,42 @@ func CalcBarnsleyJ1(
 		zy0 += 4.0 / float64(height)
 	}
 }
+
+// CalcBarnsleyJ2 calculates Barnsley J2 Mandelbrot-like set
+func CalcBarnsleyJ2(
+	width uint, height uint,
+	cx float64, cy float64,
+	maxiter uint, zimage ZImage) {
+
+	var zy0 float64 = -2.0
+	for y := uint(0); y < height; y++ {
+		var zx0 float64 = -2.0
+		for x := uint(0); x < width; x++ {
+			var zx float64 = zx0
+			var zy float64 = zy0
+			var i uint
+			for i < maxiter {
+				var zxn float64
+				var zyn float64
+				zx2 := zx * zx
+				zy2 := zy * zy
+				if zx2+zy2 > 4.0 {
+					break
+				}
+				if zx*cy+zy*cx >= 0 {
+					zxn = zx*cx - zy*cy - cx
+					zyn = zx*cy + zy*cx - cy
+				} else {
+					zxn = zx*cx - zy*cy + cx
+					zyn = zx*cy + zy*cx + cy
+				}
+				zx = zxn
+				zy = zyn
+				i++
+			}
+			zimage[y][x] = ZPixel{Iter: i, Z: complex(zx, zy)}
+			zx0 += 4.0 / float64(width)
+		}
+		zy0 += 4.0 / float64(height)
+	}
+}
