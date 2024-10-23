e535b4295e45fb9ec4d0f6994fb200f3cf4c5a8d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-10-23 08:31:46 +0200

    Updated benchmarks

--- renderer/cplx/benchmark_test.go
+++ renderer/cplx/benchmark_test.go
@@ -3,6 +3,7 @@ package cplx_test
 import (
 	"testing"
 
+	"github.com/tisnik/svitava-go/params"
 	"github.com/tisnik/svitava-go/renderer/cplx"
 )
 
@@ -13,15 +14,32 @@ const (
 )
 
 func BenchmarkCalcMandelbrot(b *testing.B) {
+	params := params.Cplx{
+		Cx0:     0,
+		Cy0:     0,
+		Maxiter: 1000,
+	}
 	zimage := cplx.NewZImage(WIDTH, HEIGHT)
 	for i := 0; i < b.N; i++ {
-		cplx.CalcMandelbrot(WIDTH, HEIGHT, 0.0, 0.0, MAXITER, zimage)
+		cplx.CalcMandelbrot(WIDTH, HEIGHT, params, zimage)
 	}
 }
 
 func BenchmarkCalcMandelbrotComplex(b *testing.B) {
+	params := params.Cplx{
+		Cx0:     0,
+		Cy0:     0,
+		Maxiter: 1000,
+	}
 	zimage := cplx.NewZImage(WIDTH, HEIGHT)
 	for i := 0; i < b.N; i++ {
-		cplx.CalcMandelbrotComplex(WIDTH, HEIGHT, 0.0, 0.0, MAXITER, zimage)
+		cplx.CalcMandelbrotComplex(WIDTH, HEIGHT, params, zimage)
+	}
+}
+
+func BenchmarkCalcJulia(b *testing.B) {
+	zimage := cplx.NewZImage(WIDTH, HEIGHT)
+	for i := 0; i < b.N; i++ {
+		cplx.CalcJulia(WIDTH, HEIGHT, 0.0, 1.0, MAXITER, zimage)
 	}
 }
