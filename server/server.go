3b115631ce2f321390fe4503e7c52a175c124564
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-01 08:27:23 +0100

    Server interface

--- server/server.go
+++ server/server.go
@@ -18,35 +18,49 @@ import (
 	"strings"
 )
 
-func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+type Server interface {
+	Serve()
+}
+
+type HTTPServer struct {
+	port uint
+}
+
+func NewHTTPServer(port uint) Server {
+	return HTTPServer{
+		port: port,
+	}
+}
+
+func (s HTTPServer) indexPageHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "web-content/index.html")
 }
 
-func newFractalPageHandler(w http.ResponseWriter, r *http.Request) {
+func (s HTTPServer) newFractalPageHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "web-content/new_fractal.html")
 }
 
-func galleryPageHandler(w http.ResponseWriter, r *http.Request) {
+func (s HTTPServer) galleryPageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
-func settingsPageHandler(w http.ResponseWriter, r *http.Request) {
+func (s HTTPServer) settingsPageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
-func staticImageHandler(w http.ResponseWriter, r *http.Request) {
+func (s HTTPServer) staticImageHandler(w http.ResponseWriter, r *http.Request) {
 	imageName := r.URL.String()
 	fileName := strings.TrimPrefix(imageName, "/image/")
 	http.ServeFile(w, r, "web-content/images/"+fileName)
 }
 
-// StartServer starts HTTP server that provides all static and dynamic data
-func StartServer(port uint) {
-	log.Printf("Starting server on port %d", port)
+// Serve starts HTTP server that provides all static and dynamic data
+func (s HTTPServer) Serve() {
+	log.Printf("Starting server on port %d", s.port)
 	/* http.Handle("/", http.FileServer(http.Dir("web-content/"))) */
 
-	http.HandleFunc("/", indexPageHandler)
-	http.HandleFunc("/new-fractal", newFractalPageHandler)
-	http.HandleFunc("/gallery", galleryPageHandler)
-	http.HandleFunc("/settings", settingsPageHandler)
+	http.HandleFunc("/", s.indexPageHandler)
+	http.HandleFunc("/new-fractal", s.newFractalPageHandler)
+	http.HandleFunc("/gallery", s.galleryPageHandler)
+	http.HandleFunc("/settings", s.settingsPageHandler)
 	//http.HandleFunc("/image/", staticImageHandler)
 	imageServer := http.FileServer(http.Dir("web-content/images/"))
 	http.Handle("/image/", http.StripPrefix("/image", imageServer))
