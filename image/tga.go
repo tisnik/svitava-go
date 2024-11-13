4d75103b1dbd81c0104693e301dd95c7bda86dec
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-13 09:37:12 +0100

    Stub for TGA raster format writer

---
+++ image/tga.go
@@ -0,0 +1,17 @@
+//
+//  (C) Copyright 2024  Pavel Tisnovsky
+//
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  which accompanies this distribution, and is available at
+//  http://www.eclipse.org/legal/epl-v10.html
+//
+//  Contributors:
+//      Pavel Tisnovsky
+//
+
+package image
+
+// WriteBMPImage writes an image represented by byte slice into file with BMP format.
+func WriteTGAImage(filename string, width uint, height uint, rawData []byte) {
+}
