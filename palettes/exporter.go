63012d39a1cb8575bdb33e65e6a1762ca1e8751f
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-01 10:22:55 +0200

    Added comments

--- palettes/exporter.go
+++ palettes/exporter.go
@@ -1,5 +1,5 @@
 //
-//  (C) Copyright 2019, 2020  Pavel Tisnovsky
+//  (C) Copyright 2019 - 2025  Pavel Tisnovsky
 //
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -12,18 +12,24 @@
 
 package palettes
 
+// SaveBinaryRGBPalette method stores RGB palette into binary file.
 func (palette *Palette) SaveBinaryRGBPalette(filename string) error {
 	return nil
 }
 
+// SaveBinaryRGBPalette method stores RGBA palette into binary file.
 func (palette *Palette) SaveBinaryRGBAPalette(filename string) error {
 	return nil
 }
 
+// SaveBinaryRGBPalette method stores RGB palette into text file compatible
+// with Fractint.
 func (palette *Palette) SaveTextRGBPalette(filename string) error {
 	return nil
 }
 
+// SaveBinaryRGBAPalette method stores RGB palette into text file that is
+// semi-compatible with Fractint.
 func (palette *Palette) SaveTextRGBAPalette(filename string) error {
 	return nil
 }
