5775a5b2beeb0cf476e1e3d45fab86285fa707c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-01 10:25:07 +0200

    Added comments

--- palettes/palette.go
+++ palettes/palette.go
@@ -1,5 +1,5 @@
 //
-//  (C) Copyright 2024  Pavel Tisnovsky
+//  (C) Copyright 2024, 2025  Pavel Tisnovsky
 //
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -12,4 +12,7 @@
 
 package palettes
 
+// Palette represents color palette used to map fractal calculation result
+// (number of iterations, for example) into RGB or RGBA color. Palettes have
+// usually 256 records, but it can be more or less.
 type Palette [][]byte
