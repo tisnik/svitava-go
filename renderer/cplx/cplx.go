3476d36805a58935ec88a06624489d9b623403dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-18 08:17:32 +0200

    Helper function to compute steps in complex plane

--- renderer/cplx/cplx.go
+++ renderer/cplx/cplx.go
@@ -11,3 +11,16 @@
 //
 
 package cplx
+
+import (
+	"github.com/tisnik/svitava-go/deepimage"
+	"github.com/tisnik/svitava-go/params"
+)
+
+func getSteps(
+	params params.Cplx,
+	image deepimage.Image) (float64, float64) {
+	stepX := float64(params.Xmax-params.Xmin) / float64(image.Resolution.Width)
+	stepY := float64(params.Ymax-params.Ymin) / float64(image.Resolution.Height)
+	return stepX, stepY
+}
