6ee8ef3ec0df4f105a0d4c8445bb7c1499442601
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-21 08:07:57 +0200

    Use Xmin, Xmax, Ymin and Ymax specified in configuration

--- renderer/cplx/barnsley_julia.go
+++ renderer/cplx/barnsley_julia.go
@@ -1,5 +1,5 @@
 //
-//  (C) Copyright 2024  Pavel Tisnovsky
+//  (C) Copyright 2024, 2025  Pavel Tisnovsky
 //
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -22,11 +22,13 @@ func CalcBarnsleyJ1(
 	params params.Cplx,
 	image deepimage.Image) {
 
+	stepX, stepY := getSteps(params, image)
+
 	cx := params.Cx0
 	cy := params.Cy0
-	var zy0 float64 = -2.0
+	var zy0 float64 = params.Ymin
 	for y := uint(0); y < image.Resolution.Height; y++ {
-		var zx0 float64 = -2.0
+		var zx0 float64 = params.Xmin
 		for x := uint(0); x < image.Resolution.Width; x++ {
 			var zx float64 = zx0
 			var zy float64 = zy0
@@ -52,9 +54,9 @@ func CalcBarnsleyJ1(
 			}
 			image.Z[y][x] = deepimage.ZPixel(complex(zx, zy))
 			image.I[y][x] = deepimage.IPixel(i)
-			zx0 += 4.0 / float64(image.Resolution.Width)
+			zx0 += stepX
 		}
-		zy0 += 4.0 / float64(image.Resolution.Height)
+		zy0 += stepY
 	}
 }
 
