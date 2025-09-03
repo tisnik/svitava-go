97c8f27411955c73402fa878c33cbeae60debbff
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-03 09:44:50 +0200

    Refactor Resolution data structure

--- deepimage/resolution.go
+++ deepimage/resolution.go
@@ -1,5 +1,5 @@
 //
-//  (C) Copyright 2024  Pavel Tisnovsky
+//  (C) Copyright 2024, 2025  Pavel Tisnovsky
 //
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -12,14 +12,39 @@
 
 package deepimage
 
+import (
+	"errors"
+	"fmt"
+)
+
+// Resolution describes the image dimensions in pixels.
 type Resolution struct {
 	Width  uint
 	Height uint
 }
 
-func NewResolution(width, height uint) Resolution {
+// NewResolution constructs a Resolution with the given width and height.
+// Width and height are expected to be positive numbers.
+func NewResolution(width, height uint) (Resolution, error) {
+	// Check for zero dimensions
+	if width == 0 {
+		return Resolution{}, errors.New("width cannot be zero")
+	}
+	if height == 0 {
+		return Resolution{}, errors.New("height cannot be zero")
+	}
+
+	// Check for reasonable maximum dimensions to prevent memory issues
+	const maxDimension = 65535 // 2^16 - 1, reasonable for image processing
+	if width > maxDimension {
+		return Resolution{}, fmt.Errorf("width %d exceeds maximum allowed dimension %d", width, maxDimension)
+	}
+	if height > maxDimension {
+		return Resolution{}, fmt.Errorf("height %d exceeds maximum allowed dimension %d", height, maxDimension)
+	}
+
 	return Resolution{
 		Width:  width,
 		Height: height,
-	}
+	}, nil
 }
