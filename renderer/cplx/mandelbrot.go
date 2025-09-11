20b0c818c610313f8b7cbdfe38d72424d6e2ec67
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-11 09:21:18 +0200

    Finished refactoring Mandelbrot fractal renderers

--- renderer/cplx/mandelbrot.go
+++ renderer/cplx/mandelbrot.go
@@ -81,68 +81,6 @@ func CalcMandelbrotComplex(
 	}
 }
 
-// CalcMandelbrotZ3 calculates Mandelbrot set z=z^3+c into the provided ZPixels
-// Calculations use complex numbers
-func CalcMandelbrotZ3(
-	params params.Cplx,
-	image deepimage.Image) {
-
-	stepX, stepY := getSteps(params, image)
-
-	var cy float64 = params.Ymin
-	for y := uint(0); y < image.Resolution.Height; y++ {
-		var cx float64 = params.Xmin
-		for x := uint(0); x < image.Resolution.Width; x++ {
-			var c complex128 = complex(cx, cy)
-			var z complex128 = complex(params.Cx0, params.Cy0)
-			var i uint
-			for i < params.Maxiter {
-				zx := real(z)
-				zy := imag(z)
-				if zx*zx+zy*zy > 4.0 {
-					break
-				}
-				z = z*z*z + c
-				i++
-			}
-			image.Z[y][x] = deepimage.ZPixel(z)
-			image.I[y][x] = deepimage.IPixel(i)
-			cx += stepX
-		}
-		cy += stepY
-	}
-}
-
-// CalcMandelbrotZ4 calculates Mandelbrot set z=z^4+c into the provided ZPixels
-// Calculations use complex numbers
-func CalcMandelbrotZ4(
-	params params.Cplx,
-	image deepimage.Image) {
-
-	var cy float64 = -1.5
-	for y := uint(0); y < image.Resolution.Height; y++ {
-		var cx float64 = -1.5
-		for x := uint(0); x < image.Resolution.Width; x++ {
-			var c complex128 = complex(cx, cy)
-			var z complex128 = complex(params.Cx0, params.Cy0)
-			var i uint
-			for i < params.Maxiter {
-				zx := real(z)
-				zy := imag(z)
-				if zx*zx+zy*zy > 4.0 {
-					break
-				}
-				z = z*z*z*z + c
-				i++
-			}
-			image.Z[y][x] = deepimage.ZPixel(z)
-			image.I[y][x] = deepimage.IPixel(i)
-			cx += 3.0 / float64(image.Resolution.Width)
-		}
-		cy += 3.0 / float64(image.Resolution.Height)
-	}
-}
-
 // CalcMandelbrotZ2pZ calculates Mandelbrot set z=z^2+z+c into the provided ZPixels
 // Calculations use complex numbers
 func CalcMandelbrotZ2pZ(
