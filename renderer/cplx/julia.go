e33e865b4f7858e63815c27dfede09fc71c612fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-22 08:37:33 +0200

    Calculate color index by using slope + shift

--- renderer/cplx/julia.go
+++ renderer/cplx/julia.go
@@ -42,7 +42,7 @@ func CalcJulia(
 				i++
 			}
 			image.Z[y][x] = deepimage.ZPixel(complex(zx, zy))
-			image.I[y][x] = deepimage.IPixel(i)
+			image.I[y][x] = deepimage.IPixel(calcIndex(params, i))
 			zx0 += stepX
 		}
 		zy0 += stepY
