f97b3c34587f59f4f19f0d2094bdb6b48ead861b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-23 08:21:35 +0200

    Calculate color in palette using slope and offset

--- renderer/cplx/lambda_julia.go
+++ renderer/cplx/lambda_julia.go
@@ -42,7 +42,7 @@ func CalcLambda(
 				i++
 			}
 			image.Z[y][x] = deepimage.ZPixel(z)
-			image.I[y][x] = deepimage.IPixel(i)
+			image.I[y][x] = deepimage.IPixel(calcIndex(params, i))
 			zx0 += 3.0 / float64(image.Resolution.Width)
 		}
 		zy0 += 3.0 / float64(image.Resolution.Height)
